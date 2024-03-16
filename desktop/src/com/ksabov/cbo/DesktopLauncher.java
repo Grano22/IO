4095428266c2ce2866e516ed987b2bbaaaecafea
Adrian Błasiak <grano22@outlook.com>
2024-03-16 12:35:48 +0100

    Game codebase - first commit

---
+++ desktop/src/com/ksabov/cbo/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.ksabov.cbo;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.ksabov.cbo.CBO;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("CBO");
+		new Lwjgl3Application(new CBO(), config);
+	}
+}
---
+++ html/src/com/ksabov/cbo/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.ksabov.cbo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ksabov.cbo.CBO;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CBO();
+        }
+}
